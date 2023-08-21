0679bf6a1e2496f403f495924d9ec99ce94e7ddf
schincharauli <52008243+schincharauli@users.noreply.github.com>
2023-08-21 11:37:21 +0400

    added products function

--- app.js
+++ app.js
@@ -5,3 +5,17 @@ const readLine = readline.createInterface({
   input: process.stdin,
   output: process.stdout,
 });
+
+
+readLine.question('Enter Product Name: ', (name)=> {
+    readLine.question('Enter Product price: ', (price) => {
+        readLine.question('Enter Product ID: ', (id) => {
+            const product = {
+                name: name,
+                price: parseFloat(price),
+                id: parseInt(id)
+            };
+
+            const products = [product];
+    })
+})
